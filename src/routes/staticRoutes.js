75873aaa6482c394a180cf1824d1f2cf5cfc41a4
thevasudev <vasudev082002@gmail.com>
2025-04-30 16:06:10 +0530

    changes

--- src/routes/staticRoutes.js
+++ src/routes/staticRoutes.js
@@ -4,5 +4,5 @@ const StaticController = require("../controller/staticController");
 
 router.post("/create", StaticController.createStatic);
 router.put("/updateStatic/:staticId", StaticController.updateStatic);
-
+router.get("/getAllStatics", StaticController.getAllStatic);
 module.exports = router;
