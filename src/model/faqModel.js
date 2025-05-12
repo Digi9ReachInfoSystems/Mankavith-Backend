4b3ccb251ca215bd83c3ba1b4fc7825b09daa495
thevasudev <vasudev082002@gmail.com>
2025-05-12 15:57:36 +0530

    changes

--- src/model/faqModel.js
+++ src/model/faqModel.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const faqSchema = new mongoose.Schema({
     question: { type: String, required: true },
     answer: { type: String, required: true },
-});
+},
+{ timestamps: true });
 
 module.exports = mongoose.model("Faq", faqSchema);
