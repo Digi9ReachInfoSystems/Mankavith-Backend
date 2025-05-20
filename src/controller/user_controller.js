129a3612fcbd85383e85f08f209a2adbd09c3260
jayanthbr29 <jayanthbr@digi9.co.in>
2025-05-20 15:17:20 +0530

    edit user

--- src/controller/user_controller.js
+++ src/controller/user_controller.js
@@ -790,3 +790,27 @@ exports.checkWishlistItem = async (req, res) => {
     });
   }
 };
+exports.editUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+    const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+      new: true,
+    });
+    res
+      .status(200)
+      .json({ success: true, message: "User updated successfully", user:updatedUser });
+  } catch (error) {
+    console.error("Error updating user:", error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Server error. Could not update user.",
+      error: error.message,
+    });
+  }
+};  
