c4dbd268e8aeb964dceacd3362c4ba9ebcbd8fde
thevasudev <vasudev082002@gmail.com>
2025-07-07 18:06:29 +0530

    changes

--- src/model/notificationModel.js
+++ src/model/notificationModel.js
@@ -4,7 +4,7 @@ const notificationSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     time : { type: String, required: true },
-    image: { type: String, required: true },
+    // image: { type: String, required: true },
     notificationType: { type: String, required: true },
 },
 {
