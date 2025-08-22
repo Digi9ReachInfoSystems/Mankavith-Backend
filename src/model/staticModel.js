0ad8e78401afa662bcf315e3b399308a4947e286
thevasudev <vasudev082002@gmail.com>
2025-08-22 18:09:39 +0530

    changes in kyc

--- src/model/staticModel.js
+++ src/model/staticModel.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 const StaticSchema = new mongoose.Schema({
-    terms: { type: String, required: true },
-    privacy: { type: String, required: true },
+    terms: { type: String, required: false },
+    privacy: { type: String, required: false },
+    refund: { type: String, required: false },
 
 });
 
