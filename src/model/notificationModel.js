7f7a0d3ec84e15b8d7c6eec701f4bb406a7f7a8b
thevasudev <vasudev082002@gmail.com>
2025-08-26 13:14:22 +0530

    changes

--- src/model/notificationModel.js
+++ src/model/notificationModel.js
@@ -6,6 +6,10 @@ const notificationSchema = new mongoose.Schema({
     time : { type: String, required: true },
     // image: { type: String, required: true },
     notificationType: { type: String, required: true },
+     read: { 
+    type: Boolean, 
+    default: false  // ← This is key: new notifications are unread by default
+  },
 },
 {
     timestamps: true
